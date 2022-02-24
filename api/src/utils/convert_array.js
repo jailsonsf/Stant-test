4fa838086232a50982f953c70f7b3a53771dc90c
jailsonsf <jailson.s.f@outlook.com>
2022-02-24 17:44:35 -0300

    Convert event duration to integer

--- api/src/utils/convert_array.js
+++ api/src/utils/convert_array.js
@@ -8,6 +8,12 @@ module.exports = {
         var event_time = arr[arr.length-1];
         arr.pop();
 
+        if (event_time.toLowerCase() == 'lightning') {
+            event_time = 5
+        } else {
+            event_time = parseInt(event_time.slice(0, -3));
+        }
+
         return { title: arr.join(' '), duration: event_time }
     }
 }
