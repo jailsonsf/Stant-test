6880c4413e595bdce3a23d6139572f7b87a328db
Jailson Soares <jailson.s.f@outlook.com>
2022-02-28 22:15:18 -0300

    Update server.js
    Remove duplicated code

--- api/src/server.js
+++ api/src/server.js
@@ -8,7 +8,6 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(cors());
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 app.use(routes);
